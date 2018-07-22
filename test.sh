12ea796888c0f3ca93050dfdc86574dabe16a9f7
Povilas Kanapickas <povilas@radix.lt>
2018-07-22 12:45:54 +0300

    test: Only check project python files for style

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-flake8 --config=config/flake8rc
+flake8 --config=config/flake8rc asm_collect.py asmtest tests
 pylint --rcfile=config/pylintrc asmtest tests
 pylint3 --rcfile=config/pylintrc asmtest tests
-isort --recursive --apply
 python -m unittest discover
 python3 -m unittest discover
+isort --recursive --apply asmtest tests asm_collect.py
