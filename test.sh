9f46db9d398f53c71135950638530a685467c5b7
Povilas Kanapickas <povilas@radix.lt>
2018-07-22 12:45:42 +0300

    test: Run python v2 by default in addition to v3

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@ flake8 --config=config/flake8rc
 pylint --rcfile=config/pylintrc asmtest tests
 pylint3 --rcfile=config/pylintrc asmtest tests
 
+python -m unittest discover
 python3 -m unittest discover
