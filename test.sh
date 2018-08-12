40d46d6127101461900dfe60b0acf9b76e4cb755
Povilas Kanapickas <povilas@radix.lt>
2018-08-13 00:34:26 +0300

    test: Add travis config

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 flake8 --config=config/flake8rc asm_collect.py asmtest tests
-pylint --rcfile=config/pylintrc asmtest tests
+pylint --rcfile=config/pylintrc asm_collect.py asmtest tests
 pylint3 --rcfile=config/pylintrc asmtest tests
 python -m unittest discover
 python3 -m unittest discover
