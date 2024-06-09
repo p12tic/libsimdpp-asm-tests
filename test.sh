c7de8534b953b93a014e07ab7dc0d4fcc39738bb
Povilas Kanapickas <povilas@radix.lt>
2024-06-09 03:54:07 +0300

    Cleanup test runner

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 flake8 --config=config/flake8rc asm_collect.py asmtest tests
 pylint --rcfile=config/pylintrc asm_collect.py asmtest tests
-pylint3 --rcfile=config/pylintrc asmtest tests
 python -m unittest discover
-python3 -m unittest discover
 isort --recursive --apply asmtest tests asm_collect.py
